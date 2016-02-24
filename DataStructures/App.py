c082307f56c71315e054c8127e62e196700b6ee1
samuel <samuel.piedagnel@googlemail.com>
2016-02-24 23:17:20 +0000

    LinkedList Python implementation

---
+++ DataStructures/App.py
@@ -0,0 +1,14 @@
+from DataStructures.LinkedList import  LinkedList
+
+list = LinkedList()
+
+list.insertEnd(12)
+list.insertEnd(122)
+list.insertEnd(3)
+list.insertStart(31)
+
+list.traverseList()
+
+list.remove(12)
+
+list.traverseList()
