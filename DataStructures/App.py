8d769f8cf1bd69c956efca98219e42a271e66a75
samuel <samuel.piedagnel@googlemail.com>
2016-02-25 23:13:32 +0000

    LinkedList Python implementation - aaded Queue

--- DataStructures/App.py
+++ DataStructures/App.py
@@ -1,5 +1,6 @@
 from DataStructures.LinkedList import  LinkedList
 from DataStructures.Stack import  Stack
+from DataStructures.Queue import  Queue
 
 list = LinkedList()
 
@@ -25,3 +26,15 @@ stack.push(3)
 print(stack.pop())
 print(stack.pop())
 print(stack.size())
+
+print("\n")
+print("Queue:")
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.size())
